2361d9b82b985e18607f34beb93315e5b1bc2d3a
Anduin Xue <anduin@aiursoft.com>
2026-04-07 11:33:00 +0000

    Update MarketingNavbar and MarketingFooter to reflect EventsRecorder branding and links

--- src/Aiursoft.EventsRecorder/Views/Shared/Components/MarketingNavbar/MarketingNavbarViewModel.cs
+++ src/Aiursoft.EventsRecorder/Views/Shared/Components/MarketingNavbar/MarketingNavbarViewModel.cs
@@ -2,6 +2,6 @@ namespace Aiursoft.EventsRecorder.Views.Shared.Components.MarketingNavbar;
 
 public class MarketingNavbarViewModel
 {
-    public string ProjectName { get; set; } = "Aiursoft Template";
+    public string ProjectName { get; set; } = "EventsRecorder";
     public string LogoUrl { get; set; } = "/logo.svg";
 }
